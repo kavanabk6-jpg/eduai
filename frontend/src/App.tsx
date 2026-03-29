413abb009a81df855d4b5080d8910e0e99847e70
kavanabk6-jpg <kavanabk6@gmail.com>
2026-03-29 14:22:17 +0530

    Create main React application component for EduAI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to EduAI</h1>
+      <p>This is the main application component for EduAI.</p>
+    </div>
+  );
+};
+
+export default App;
